68de4cdcc49ac5234373b31437d745f14cfa9daa
mohammad salmani <mohammadsalmani28@gmail.com>
2022-04-22 13:07:42 +0430

    fix: bug fixed in user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,8 @@ class UserFactory extends Factory
     {
         return [
             'type' => $this->faker->randomElement([User::TYPE_ADMIN, User::TYPE_USER]),
-            'name' => $this->faker->name(),
+            'first_name' => $this->faker->name(),
+            'last_name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => 'secret',
