8d36e3cb915e7f46e2d235980d9aa35eda223af4
mohammad salmani <mohammadsalmani28@gmail.com>
2022-04-25 12:42:57 +0430

    add: new route added for everyone to access games

--- routes/frontend/home.php
+++ routes/frontend/home.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Domains\Games\Http\Controllers\GameController;
 use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\Frontend\TermsController;
 use Illuminate\Support\Facades\Route;
@@ -20,4 +21,11 @@ Route::get('terms', [TermsController::class, 'index'])
     ->breadcrumbs(function (Trail $trail) {
         $trail->parent('frontend.index')
             ->push(__('Terms & Conditions'), route('frontend.pages.terms'));
+    });
+
+Route::get('games/{game}', [GameController::class, 'run'])
+    ->name('games.show')
+    ->breadcrumbs(function (Trail $trail, $game) {
+        $trail->parent('frontend.index')
+            ->push($game->name, route('frontend.games.show', $game));
     });
